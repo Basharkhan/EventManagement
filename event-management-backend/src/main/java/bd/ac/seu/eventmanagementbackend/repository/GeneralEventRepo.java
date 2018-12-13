1ecc70d4e756b2471a7ba3f8bf40c038510fa0e0
Bashar <basharkhan71@gmail.com>
2018-12-13 21:09:50 +0600

    removing a directory

--- event-management-backend/src/main/java/bd/ac/seu/eventmanagementbackend/repository/GeneralEventRepo.java
+++
@@ -1,7 +0,0 @@
-package bd.ac.seu.eventmanagementbackend.repository;
-
-import bd.ac.seu.eventmanagementbackend.model.GeneralEvent;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface GeneralEventRepo extends MongoRepository<GeneralEvent, String> {
-}
--- event-management-backend/src/main/java/bd/ac/seu/eventmanagementbackend/repository/SpecialEventRepo.java
+++
@@ -1,7 +0,0 @@
-package bd.ac.seu.eventmanagementbackend.repository;
-
-import bd.ac.seu.eventmanagementbackend.model.SpecialEvent;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface SpecialEventRepo extends MongoRepository<SpecialEvent, String> {
-}
